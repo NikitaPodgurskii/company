5e619c4d96a0b0858fbd7121324929dbc60d217c
n.podgurskiy <mortarionggl@gmail.com>
2023-11-08 11:52:38 +0200

    Data structure & tests

--- src/telran/company/service/CompanyServiceImp.java
+++ src/telran/company/service/CompanyServiceImp.java
@@ -1,18 +1,26 @@
 package telran.company.service;
 
+import java.time.LocalDate;
+import java.util.*;
 import java.util.List;
+import java.util.Set;
 
 import telran.company.dto.DepartmentAvgSalary;
 import telran.company.dto.Employee;
 import telran.company.dto.SalaryIntervalDistribution;
 
 public class CompanyServiceImp implements CompanyService {
-
+	//HashMap самый оптимальный выбор при сложности алгоритма 1
+	HashMap<Long, Employee> employeesMap = new HashMap<>();//для поиска по id
+	HashMap<String, Set<Employee>> employeesDepartment = new HashMap<>();//для поиска по отделу, key=department, value=set of empl of dep
+	TreeMap<Integer, Set<Employee>> employeesSalary = new TreeMap<>();//TreeMap оптимальный, если нужно получить диапазон, key=salary, value=set of empl having salary value
+	TreeMap<LocalDate, Set<Employee>> employeesAge = new TreeMap<>();//key=birth date, value=set of empl born at the date
+	//если константное обращение - сложность сразу логарифмическая
 	@Override
 	//добавляет служащего, но если есть такой же ID - бросить exeption, вернуть
 	//ссылку на добавленного служащего
 	public Employee hireEmployee(Employee empl) {
-		
+		// TODO Auto-generated method stub O[1]
 		return null;
 	}
 
@@ -20,14 +28,14 @@ public class CompanyServiceImp implements CompanyService {
 	//удаляет служащего с данным id
 	//если такого ид нет - метод должен бросить IllegalStateException
 	public Employee fireEmployee(long id) {
-		// TODO Auto-generated method stub
+		// TODO  O[1]
 		return null;
 	}
 
 	@Override
 	//возвращает ссылку на Employee по id. Если такого id нет - метод возвращает null
 	public Employee getEmployee(long id) {
-		// TODO Auto-generated method stub
+		// TODO  O[1]
 		return null;
 	}
 
@@ -35,61 +43,61 @@ public class CompanyServiceImp implements CompanyService {
 	//возвращает лист с сотрудниками по отделам
 	//если никого из них нет в отделе, вернуть пустой лист
 	public List<Employee> getEmployeesByDepartment(String department) {
-		// TODO Auto-generated method stub
+		// TODO  O[1]
 		return null;
 	}
 
 	@Override
 	public List<Employee> getAllEmployees() {
-		// TODO Auto-generated method stub
+		// TODO O[N]
 		return null;
 	}
 
 	@Override
 	public List<Employee> getEmployeesBySalary(int salaryTo, int salaryFrom) {
-		// TODO Auto-generated method stub
+		// TODO O[LogN]
 		return null;
 	}
 
 	@Override
 	public List<Employee> getEmployeeByAge(int ageFrom, int ageTo) {
-		// TODO Auto-generated method stub
+		// TODO O[LogN]
 		return null;
 	}
 
 	@Override
 	public List<DepartmentAvgSalary> salaryDisributionByDepartments() {
-		// TODO Auto-generated method stub
+		// TODO O[N]
 		return null;
 	}
 
 	@Override
 	public List<SalaryIntervalDistribution> getSalaryDisribution(int intetval) {
-		// TODO Auto-generated method stub
+		// TODO O[N]
 		return null;
 	}
 
 	@Override
 	public Employee updateDepartment(long id, String newDepartment) {
-		// TODO Auto-generated method stub
+		// TODO O[1]
 		return null;
 	}
 
 	@Override
 	public Employee updateSalary(long id, int newSalary) {
-		// TODO Auto-generated method stub
+		// TODO O[LogN]
 		return null;
 	}
 
 	@Override
 	public void save(String filePath) {
-		// TODO Auto-generated method stub
+		// TODO O[N]
 
 	}
 
 	@Override
 	public void restore(String file) {
-		// TODO Auto-generated method stub
+		// TODO O[N]
 
 	}
 
--- src/telran/company/test/CompanyTest.java
+++ src/telran/company/test/CompanyTest.java
@@ -8,13 +8,19 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import telran.company.dto.DepartmentAvgSalary;
 import telran.company.dto.Employee;
+import telran.company.dto.SalaryIntervalDistribution;
 import telran.company.service.CompanyService;
 import telran.company.service.CompanyServiceImp;
 
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class) //для определения порядка тестов
 class CompanyTest {
 	private static final long ID1 = 123;
 	private static final long ID2 = 124;
@@ -37,6 +43,7 @@ class CompanyTest {
 	private static final String DEPARTMENT3 = "dep3";
 	private static final long ID6 = 1000;
 	private static final String DEPARTMENT6 = "depunknown";
+	private static final String FILE_PATH = "test.data";
 	Employee empl1 = new Employee(ID1, "name1", SALARY1, DEPARTMENT1, DATE1);
 	Employee empl2 = new Employee(ID2, "name2", SALARY2, DEPARTMENT1, DATE2);
 	Employee empl3 = new Employee(ID3, "name3", SALARY3, DEPARTMENT2, DATE3);
@@ -104,29 +111,35 @@ class CompanyTest {
 		Employee[] expectedDep2 = {empl3, empl4};
 		List<Employee> list1 = company.getEmployeesByDepartment(DEPARTMENT1);
 		List<Employee> list2 = company.getEmployeesByDepartment(DEPARTMENT2);
+		/*
 		Employee[] actualDep1 = list1.toArray(new Employee[] {});//из листа1 получить массив 
 		Employee[] actualDep2 = list2.toArray(new Employee[] {});
 		Arrays.sort(actualDep1);
 		Arrays.sort(actualDep2);
-		assertTrue(company.getEmployeesByDepartment(DEPARTMENT6).isEmpty());//проверяем, что метод вернет пустой список
 		assertArrayEquals(expectedDep1, actualDep1);//проверяем порядок сотрудников здесь и в методе одинаковый
 		assertArrayEquals(expectedDep2, actualDep2);
+		*/
+		assertTrue(company.getEmployeesByDepartment(DEPARTMENT6).isEmpty());//проверяем, что метод вернет пустой список
+		runListTest(expectedDep1, list1);//упрощение записи благодаря введенному ниже методу. Полная верся в комментарии выше
+		runListTest(expectedDep2, list2);
 	}
 
 	@Test
-	//дз (получить массив со всеми и отсортировать)
 	void testGetAllEmployees() {
+		/*
 		List<Employee> list =  company.getAllEmployees();
 		Employee[] actual = list.toArray(new Employee[] {});
 		Arrays.sort(actual);
 		assertArrayEquals(employees, actual);
+		*/
+		runListTest(employees, company.getAllEmployees());
 		
 		
 	}
 
 	@Test
-	//дз (3 диапазона как ниже)
 	void testGetEmployeesBySalary() {
+		/*
 		List<Employee> listAll = company.getEmployeesBySalary(0, 10000);
 		Employee[] actualAll = listAll.toArray(new Employee[] {});
 		Arrays.sort(actualAll);
@@ -138,11 +151,17 @@ class CompanyTest {
 		Employee[] expected2_3 = {empl2, empl3};
 		Arrays.sort(actual2_3);
 		assertArrayEquals(expected2_3, actual2_3);
+		*/
+		runListTest(employees, company.getEmployeesBySalary(0, Integer.MAX_VALUE));//подходит для всех
+		runListTest(new Employee[0], company.getEmployeesBySalary(10000, Integer.MAX_VALUE));//ни для кого, Employee[0] = пустой массив
+		runListTest(new Employee[] {empl1, empl2}, company.getEmployeesBySalary(SALARY1, SALARY3));//между двумя
+		
 		
 	}
 
 	@Test
 	void testGetEmployeeByAge() {
+		/*
 		List<Employee> listAll = company.getEmployeeByAge(0, 100);
 		Employee[] actualAll = listAll.toArray(new Employee[] {});
 		Arrays.sort(actualAll);
@@ -154,7 +173,10 @@ class CompanyTest {
 		Employee[] expected2_3 = {empl2, empl3};
 		Arrays.sort(actual2_3);
 		assertArrayEquals(expected2_3, actual2_3);
-		
+		*/
+		runListTest(employees, company.getEmployeeByAge(0, 100));//все
+		runListTest(new Employee[0], company.getEmployeeByAge(90, 100));//ни одного
+		runListTest(new Employee[] {empl2, empl2}, company.getEmployeeByAge(getAge(DATE3), getAge(DATE1)));//между
 	}
 
 	@Test
@@ -171,34 +193,57 @@ class CompanyTest {
 	}
 
 	@Test
-	void testGetSalaryDisribution() {
-		fail("Not yet implemented");
+	void testGetSalaryDisribution() { //количество служащих, получающих зп в диапазонах
+		int interval = 2000;
+		List<SalaryIntervalDistribution> distribution = company.getSalaryDisribution(interval);//задаем диапазон
+		SalaryIntervalDistribution[] expectedDistribution = { //ожидаемое распределение
+			new SalaryIntervalDistribution(SALARY1, SALARY1 + interval, 2) ,
+			new SalaryIntervalDistribution(SALARY3, SALARY3 + interval, 2) ,
+			new SalaryIntervalDistribution(SALARY5, SALARY5 + interval, 1)
+		};
+		assertArrayEquals(expectedDistribution, distribution.toArray(new SalaryIntervalDistribution[0]));
+		
 	}
 
 	@Test
 	void testUpdateDepartment() {
-		fail("Not yet implemented");
+		assertEquals(empl2, company.updateDepartment(ID2, DEPARTMENT2));//вернуть ссылку на служащего, которому сменили отдел
+		runListTest(new Employee[] {empl1}, company.getEmployeesByDepartment(DEPARTMENT1));//сравнить обновленное распределение по отделам после перемещения сотрудника
+		runListTest(new Employee[] {empl2, empl3, empl4}, company.getEmployeesByDepartment(DEPARTMENT2));
 	}
 
 	@Test
 	void testUpdateSalary() {
-		fail("Not yet implemented");
+		assertEquals(empl2, company.updateSalary(ID2, SALARY3));//аналогично обновлению отдела
+		runListTest(new Employee[] {empl1}, company.getEmployeesBySalary(SALARY1, SALARY3));
+		runListTest(new Employee[] {empl2, empl3, empl4}, company.getEmployeesBySalary(SALARY3, SALARY5));
 	}
 
 	@Test
+	@Order(1) //порядок теста, этот будет первым
 	void testSave() {
-		fail("Not yet implemented");
+		company.save(FILE_PATH);//сохраняем все данные в файл
 	}
 
 	@Test
+	@Order(2)
 	void testRestore() {
-		fail("Not yet implemented");
+		CompanyService companySaved = new CompanyServiceImp();
+		companySaved.restore(FILE_PATH);//восстанавливаем данные из файла
+		runListTest(employees, companySaved.getAllEmployees());//сравниваем текущий массив сотрудников с выгруженным из файла
 	}
+	
 	private int getAge(LocalDate birthdate) {
 		int result = (int) ChronoUnit.YEARS.between(birthdate, LocalDate.now()); //в чем будет разница между данными (тут в годах между текущей и годом рождения)
 		return result;
 	}
 	
+	private void runListTest(Employee[] expected, List<Employee> list) { //для повторяющихся действий ожидаемых массивов
+		//так облегчаем алгоритм для остальных тестов
+		Employee[] actual = list.toArray(new Employee[] {});//получаем массив actual
+		Arrays.sort(actual);//сортируем
+		assertArrayEquals(expected, actual);//сравниваем ожидание/реальность
+	}
 	
 	
 	
