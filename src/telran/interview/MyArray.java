77796a5d35548ffe0c00d27a0ac44862dba1fc90
Yuragranovsky <yuragranovsky@gmail.com>
2023-11-13 14:24:06 +0200

    MyArray - stub & MyArrayTests - tests

---
+++ src/telran/interview/MyArray.java
@@ -0,0 +1,47 @@
+package telran.interview;
+
+import java.util.HashMap;
+
+/**
+ * All methods of the class should have complexity O[1]
+ * @author User
+ *
+ * @param <T>
+ */
+public class MyArray {
+	//TODO fields of the class (encapsulation)
+	/********************************************/
+	/**
+	 * constructor
+	 * @param size - length of the array
+	 */
+	public MyArray(int size) {
+		//TODO constructor
+	}
+	/**
+	 * sets all array's elements with a given value
+	 * @param value
+	 */
+	public void setAll(int value) {
+		
+		//TODO
+	}
+	/**
+	 *
+	 * @param index
+	 * @return value at given index or null if index is wrong
+	 */
+	public int get(int index) {
+		//TODO
+		return -1;
+	}
+	/**
+	 * sets a given value at a given index
+	 * throws IndexOutOfBoundsException in the case of wrong index
+	 * @param index
+	 * @param value
+	 */
+	public void set(int index, int value) {
+		//TODO
+	}
+}
